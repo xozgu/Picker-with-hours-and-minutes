92acea2487664e67985aa22835c1def8e4f61022
Ozgu Ozden <ozgu@theshell.co.jp>
2022-04-12 17:24:19 +0900

    Initial Commit

---
+++ Picker with hours and minutes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Picker with hours and minutes
+//
+//  Created by Ozgu Ozden on 2022/04/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Picker with hours and minutes/Picker_with_hours_and_minutesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Picker_with_hours_and_minutesApp.swift
+//  Picker with hours and minutes
+//
+//  Created by Ozgu Ozden on 2022/04/12.
+//
+
+import SwiftUI
+
+@main
+struct Picker_with_hours_and_minutesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
