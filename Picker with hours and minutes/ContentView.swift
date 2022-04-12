515327017bbdfd88ef1cfdedc75867a423d11a40
Ozgu Ozden <ozgu@theshell.co.jp>
2022-04-12 18:11:21 +0900

    Multiple wheelpickers side-by-side

--- Picker with hours and minutes/ContentView.swift
+++ Picker with hours and minutes/ContentView.swift
@@ -8,9 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var hours = 0
+    @State private var minutes = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            GeometryReader { geometry in
+                HStack(spacing: 0) {
+                    Picker("", selection: $hours){
+                        ForEach(0..<25, id: \.self) { i in
+                            Text("\(i) hours").tag(i)
+                        }
+                    }
+                    .pickerStyle(WheelPickerStyle())
+                    .frame(width: geometry.size.width/2)
+                    .compositingGroup()
+                    .clipped()
+                    
+                    
+                    Picker("", selection: $minutes){
+                        ForEach(0..<60, id: \.self) { i in
+                            Text("\(i) min").tag(i)
+                        }
+                    }
+                    .pickerStyle(WheelPickerStyle())
+                    .frame(width: geometry.size.width/2)
+                    .compositingGroup()
+                    .clipped()
+                    
+                }
+            }
+            .navigationTitle("\(hours) hours \(minutes) minutes")
+
+        }
     }
 }
 
